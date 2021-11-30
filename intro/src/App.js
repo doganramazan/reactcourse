7585689f9ce2a45041b539a868a295a35d0d9db6
Ramazan Doğan <doganrmzn40@gmail.com>
2021-12-01 01:12:40 +0300

    states_and_props_olusturuldu

---
+++ intro/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import CategoryList from "./CategoryList";
+import Navi from "./Navi";
+import ProductList from "./ProductList";
+import { Container, Row, Col } from "reactstrap";
+
+export default class App extends Component {
+  state={currentCategory:""}
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName })
+  }
+  render() {
+    let productInfo = { title: "ProductList" }
+    let categoryInfo = { title: "CategoryList" }
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi />
+          </Row>
+          <Row>
+            <Col xs="3">
+              <CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo} />
+            </Col>
+            <Col xs="3">
+              <ProductList currentCategory={this.state.currentCategory} info={productInfo} />
+            </Col>
+          </Row>
+        </Container>
+
+
+      </div>
+    );
+  }
+}
